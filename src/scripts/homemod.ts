9c46886b38cfb911b0a24627abac5bd1ae1ed0a3
Saransh-255 <saranshsharma255@gmail.com>
2022-03-19 15:07:07 +0530

    homepage button new design

--- src/scripts/homemod.ts
+++ src/scripts/homemod.ts
@@ -1,8 +1,30 @@
 function appendModerationButtons() {
-  const questions = document.querySelectorAll(".brn-feed-items > div[data-testid = 'feed-item'] .brn-feed-item__points .brn-feed-item__grid-gap-bottom");
+  const questions = document.querySelectorAll(".brn-feed-items > div[data-testid = 'feed-item']");
   for (let questionBox of Array.from(questions)) {
+    try{
+    let actionlist = questionBox.querySelector(".sg-actions-list__hole.sg-actions-list__hole--to-right");
     if (questionBox.querySelector(".mod-button")) continue;
-    questionBox.insertAdjacentHTML("beforeend", `<button class="mod-button"><div class="sg-icon sg-icon--dark sg-icon--x32"><svg class="sg-icon__svg"><use xlink:href="#icon-shield"></use></svg></div></button>`);
+    actionlist.insertAdjacentHTML("afterend", `<button class="mod-button sg-button--outline"><div class="sg-icon sg-icon--dark sg-icon--x32"><svg class="sg-icon__svg"><use xlink:href="#icon-shield"></use></svg></div></button>`);
+    actionlist.querySelector("a").innerHTML = '<div class="sg-icon sg-icon--dark sg-icon--x32"><svg class="sg-icon__svg"><use xlink:href="#icon-plus"></use></svg></div>'
+    }catch(err){
+      if(questionBox.id !== "noanswer"){
+      questionBox.id = 'noanswer'
+      questionBox.querySelector(".brn-feed-item__footer .sg-actions-list").insertAdjacentHTML("afterend", `<div class = "sg-actions-list__hole sg-actions-list__hole--to-right"<button class="mod-button sg-button--outline"><div class="sg-icon sg-icon--dark sg-icon--x32"><svg class="sg-icon__svg"><use xlink:href="#icon-shield"></use></svg></div></button></div>`);
+      }
+    }
+
+    questionBox.querySelector(" .mod-button").addEventListener("click", function(){
+      document.body.insertAdjacentHTML("beforeend", `
+      <div class = "modal_back">
+        <div class = "modal">
+          <div class="modal_close" role="button" tabindex="0"><div class="sg-icon sg-icon--icon-gray-50 sg-icon--x24"><svg class="sg-icon__svg" role="img" aria-labelledby="title-close-r5g6x" focusable="false"><title id="title-close-r5g6x">close</title><use xlink:href="#icon-close" aria-hidden="true"></use></svg></div></div>
+        </div>
+      </div>
+      `)
+      document.querySelector(".modal_close").addEventListener("click", function(){
+        document.querySelector(".modal_back").remove()
+    })
+    })
   }
 }
   
