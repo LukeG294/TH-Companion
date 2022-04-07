cbd7af5ea22dd771db9382817fbccd2596a936bf
Saransh-255 <saranshsharma255@gmail.com>
2022-04-08 02:33:05 +0530

    icons

--- src/scripts/styleguide.ts
+++ src/scripts/styleguide.ts
@@ -1,4 +1,3 @@
 document.querySelector("html").insertAdjacentHTML("afterbegin", /*html*/`
     <link href="https://styleguide.brainly.com/197.0.0/style-guide.css" rel="stylesheet"/>
-    <script src="https://styleguide.brainly.com/images/icons-6f3ca24ce2.js"></script>
 `)
