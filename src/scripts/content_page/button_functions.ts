ebca105cd66533dbd1b39ea9b010a65e4c502396
Luke Graham <62516707+LukeG294@users.noreply.github.com>
2022-04-08 15:08:41 -0700

    Update button_functions.ts

--- src/scripts/content_page/button_functions.ts
+++ src/scripts/content_page/button_functions.ts
@@ -52,7 +52,7 @@ export async function showDelrsn(){
     fetch(`https://brainly.com/api/28/moderate_tickets/expire`,{method: "POST", body:`{"model_id":${id},"model_type_id":1,"schema":"moderation.ticket.expire"}`})
 
     let del_reasons = res.data.delete_reasons.task;
-    console.log(del_reasons)
+    
     //inserting primary deletion reasons
 
     for(let i = 0; i < del_reasons.length; i++){
@@ -130,4 +130,90 @@ export async function confirmDeletionQuestions(){
         })
   
   }
+}
+
+export async function confirmDeletionAnswers(){
+  let checkBoxes = document.getElementsByClassName("contentCheckboxes")
+  let idsToDelete = []
+  for (let i = 0; i < checkBoxes.length; i++) {
+      //@ts-ignore
+      if (String(checkBoxes[i].checked) === "true") {
+          //@ts-ignore
+          let link = checkBoxes[i].closest("tr").getElementsByTagName('a')[0].href
+          let id = link.split("/")[4]
+          idsToDelete.push(id)
+      } 
+  }
+  
+  let answerIDtoDelete = []
+  for (let i = 0; i < idsToDelete.length; i++) {
+    
+    let questionID = idsToDelete[i]
+    let res = await fetch(`https://brainly.com/api/28/moderation_new/get_content`, { method: "POST",body: (`{"model_type_id":1,"model_id":${questionID},"schema":"moderation.content.get"}`)}).then(data => data.json());
+    await fetch(`https://brainly.com/api/28/moderate_tickets/expire`,{method: "POST", body:`{"model_id":${questionID},"model_type_id":1,"schema":"moderation.ticket.expire"}`})
+    let answers = res.data.responses
+    let times = 0
+    
+   
+    if (answers.length === 1){
+      times = 1
+    } else {
+      times = 2
+    }
+    for (let x = 0; x < times; x++) {
+      
+      let user = String(answers[x]["user_id"])
+      if (user === String(window.location.href.split("/")[5])){
+        answerIDtoDelete.push(answers[x]["id"])
+      }
+    }
+    
+  }
+
+  let success = 0
+  let fail = 0
+  for (let i = 0; i < answerIDtoDelete.length; i++) {
+   
+      let model_type_id = 2;
+      let type = "response"
+      //@ts-expect-error
+      let reason = document.querySelectorAll(".deletion-reason")[0].value
+      //@ts-expect-error
+      let warn = document.querySelector("#warn").checked
+      //@ts-expect-error
+      let take_point = document.querySelector("#pts").checked
+      let response = await fetch(`https://brainly.com/api/28/moderation_new/delete_${type}_content`, {method: "POST",body:JSON.stringify({"reason_id":2,"reason":reason,"give_warning":warn,"take_points": take_point,"schema":`moderation.${type}.delete`,"model_type_id":model_type_id,"model_id":answerIDtoDelete[i]})}).then(data => data.json());;
+      console.log(response)
+      if (response["success"] === true){
+        success+=1
+      } else {
+        fail +=1
+      }
+  }
+
+  if (fail > 0){
+    let banner = document.createElement('div')
+    document.querySelector("#flash-msg").appendChild(banner)
+    banner.outerHTML = `<div aria-live="assertive" class="sg-flash" role="alert">
+                <div class="sg-flash__message sg-flash__message--error">
+                <div class="sg-text sg-text--small sg-text--bold sg-text--to-center">${success} removed, ${fail} had an error. Make sure they weren't already deleted.</div>
+                </div>
+            </div>`
+    banner.addEventListener("click",function(){
+      this.remove();
+    })
+  } else {
+    let banner = document.createElement('div')
+    document.querySelector("#flash-msg").appendChild(banner)
+    banner.outerHTML = `<div aria-live="assertive" class="sg-flash" role="alert">
+                <div class="sg-flash__message sg-flash__message--success">
+                <div class="sg-text sg-text--small sg-text--bold sg-text--to-center">${success} removed successfully!</div>
+                </div>
+            </div>`
+    banner.addEventListener("click",function(){
+      this.remove();
+    })
+  }
+  
+  
 }
