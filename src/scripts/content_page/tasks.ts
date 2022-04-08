a891b4f72dc0e0cb235b71383aaa83f8b93e8693
Saransh-255 <saranshsharma255@gmail.com>
2022-04-09 02:52:40 +0530

    improved content page

--- src/scripts/content_page/tasks.ts
+++ src/scripts/content_page/tasks.ts
@@ -14,32 +14,36 @@ import {
     confirmDeletionQuestions
 } from "./button_functions"
 
-let content = document.querySelectorAll("#content-old > div:nth-child(2) > div:nth-child(25) > table > tbody > tr")
-for (let i = 0; i < content.length; i++) {
-    content[i].insertAdjacentHTML('beforeend', `
-    <div class="sg-space-x-m"><label class="sg-checkbox" for="mmm41eh8ef8"><input type="checkbox" class="sg-checkbox__element contentCheckboxes" id="${i}">
-        <div class="sg-checkbox__ghost" aria-hidden="true">
-        <div class="sg-icon sg-icon--adaptive sg-icon--x16"><svg class="sg-icon__svg" role="img" aria-labelledby="title-check-255xyo" focusable="false"><text id="title-check-255xyo" hidden="">check</text>
-            <use xlink:href="#icon-check" aria-hidden="true"></use>
-            </svg>
+
+function addButtons(){
+    let content = document.querySelectorAll("#content-old > div:nth-child(2) > div:nth-child(25) > table > tbody > tr")
+    for (let i = 0; i < content.length; i++) {
+        content[i].insertAdjacentHTML('beforeend', `
+        <div class="sg-space-x-m"><label class="sg-checkbox" for="mmm41eh8ef8"><input type="checkbox" class="sg-checkbox__element contentCheckboxes" id="${i}">
+            <div class="sg-checkbox__ghost" aria-hidden="true">
+            <div class="sg-icon sg-icon--adaptive sg-icon--x16"><svg class="sg-icon__svg" role="img" aria-labelledby="title-check-255xyo" focusable="false"><text id="title-check-255xyo" hidden="">check</text>
+                <use xlink:href="#icon-check" aria-hidden="true"></use>
+                </svg>
+            </div>
         </div>
-    </div>
-    </label></div>
-`)}
+        </label></div>
+    `)}
 
-let buttonArea = document.querySelector("#content-old > div:nth-child(3) > p")
-    //if you want to add permissions for each button later, do it here (below)
-let url = String(window.location.href)
-buttonArea.insertAdjacentHTML('afterend', deletion_menu())
-buttonArea.insertAdjacentHTML('afterend', confirm_selected())
-buttonArea.insertAdjacentHTML('afterend', delete_selected_questions())
-buttonArea.insertAdjacentHTML('afterend', copy_links())
-buttonArea.insertAdjacentHTML('afterend', toggle_selected())
-buttonArea.insertAdjacentHTML('afterend', select_all())
+    let buttonArea = document.querySelector("#content-old > div:nth-child(3) > p")
+        //if you want to add permissions for each button later, do it here (below)
+    let url = String(window.location.href)
+    buttonArea.insertAdjacentHTML('afterend', deletion_menu())
+    buttonArea.insertAdjacentHTML('afterend', confirm_selected())
+    buttonArea.insertAdjacentHTML('afterend', delete_selected_questions())
+    buttonArea.insertAdjacentHTML('afterend', copy_links())
+    buttonArea.insertAdjacentHTML('afterend', toggle_selected())
+    buttonArea.insertAdjacentHTML('afterend', select_all())
 
-document.getElementById("selectAll").addEventListener("click", function(){selectAll()})
-document.getElementById("toggleSelected").addEventListener("click", function(){toggleSelection()})
-document.getElementById("copyLinks").addEventListener("click", function(){copyLinks()})
-document.querySelector("#deleteSelectedQuestions").addEventListener("click", function(){showDelrsn()})
-document.querySelector("#delete").addEventListener("click",function(){confirmDeletionQuestions()})
+    document.getElementById("selectAll").addEventListener("click", function(){selectAll()})
+    document.getElementById("toggleSelected").addEventListener("click", function(){toggleSelection()})
+    document.getElementById("copyLinks").addEventListener("click", function(){copyLinks()})
+    document.querySelector("#deleteSelectedQuestions").addEventListener("click", function(){showDelrsn()})
+    document.querySelector("#delete").addEventListener("click",function(){confirmDeletionQuestions()})
+}
 
+addButtons()
