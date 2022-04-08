30bbdf45edb25a59e3dc69f403932f5d677686c4
Luke Graham <62516707+LukeG294@users.noreply.github.com>
2022-04-08 10:57:47 -0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,10 @@ const config = {
     ...MakeEntries("./src/scripts/styleguide-icons.ts", "content-scripts/js", "icons"),
     ...MakeEntries("./src/scripts/styleguide.ts", "content-scripts/js", "styleguide"),
     ...MakeEntries("./src/scripts/question_page/question_page.ts", "content-scripts/js", "question_page"),
-    ...MakeEntries("./src/scripts/content_page/content_page.ts", "content-scripts/js", "content_page")
+    ...MakeEntries("./src/scripts/content_page/tasks.ts", "content-scripts/js", "tasks"),
+    ...MakeEntries("./src/scripts/content_page/responses.ts", "content-scripts/js", "responses"),
+    ...MakeEntries("./src/scripts/content_page/comments.ts", "content-scripts/js", "comments")
+
   },
   output: {
     path: path.resolve(__dirname, "dist"),
