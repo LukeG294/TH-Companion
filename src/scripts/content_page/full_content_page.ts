25a52ba22e27b9fe7062ae66b148e24efbf288a3
Saransh-255 <saranshsharma255@gmail.com>
2022-04-09 23:28:38 +0530

    faster loading times

--- src/scripts/content_page/full_content_page.ts
+++
@@ -1,6 +0,0 @@
-let url = String(window.location.href)
-if (url.includes("tasks") || url.includes("responses") || url.includes("comments_tr")){
- //pass
-} else {
-    window.location.href += "/tasks"
-}
